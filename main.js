b91c75855bfcd851bf6ec2b17b8a22827df6d7c2
leery <contact.leery@gmail.com>
2022-11-23 11:01:20 +0300

    develop start

---
+++ main.js
@@ -0,0 +1,7 @@
+import './styles/style.scss'
+import javascriptLogo from './javascript.svg'
+import { setupCounter } from './counter.js'
+
+// alert( 'You need to creat an Acount to follow all movement!' );
+
+
